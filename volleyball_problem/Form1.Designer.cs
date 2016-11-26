423f5363043f539f1c2d776cc515095f7a1b7ccc
meikelwis <meikelwis.wijaya@gmail.com>
2016-11-26 10:34:18 +0700

    Volleyball Problem

---
+++ volleyball_problem/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace volleyball_problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.BtnHitung = new System.Windows.Forms.Button();
+            this.TxtHasil = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(12, 37);
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(100, 20);
+            this.Txt1.TabIndex = 0;
+            // 
+            // Txt2
+            // 
+            this.Txt2.Location = new System.Drawing.Point(13, 84);
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(100, 20);
+            this.Txt2.TabIndex = 1;
+            // 
+            // BtnHitung
+            // 
+            this.BtnHitung.Location = new System.Drawing.Point(13, 123);
+            this.BtnHitung.Name = "BtnHitung";
+            this.BtnHitung.Size = new System.Drawing.Size(75, 23);
+            this.BtnHitung.TabIndex = 2;
+            this.BtnHitung.Text = "Hitung";
+            this.BtnHitung.UseVisualStyleBackColor = true;
+            this.BtnHitung.Click += new System.EventHandler(this.BtnHitung_Click);
+            // 
+            // TxtHasil
+            // 
+            this.TxtHasil.Location = new System.Drawing.Point(13, 169);
+            this.TxtHasil.Name = "TxtHasil";
+            this.TxtHasil.Size = new System.Drawing.Size(100, 20);
+            this.TxtHasil.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.TxtHasil);
+            this.Controls.Add(this.BtnHitung);
+            this.Controls.Add(this.Txt2);
+            this.Controls.Add(this.Txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt1;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.Button BtnHitung;
+        private System.Windows.Forms.TextBox TxtHasil;
+    }
+}
+
---
+++ volleyball_problem/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volleyball_problem
+{
+    public partial class Form1 : Form
+    {
+        public int a, b;
+        public const int mods = 1000000007;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Hitung(int a,int b)
+        {
+
+            a = Convert.ToInt32(Txt1.Text);
+            b = Convert.ToInt32(Txt2.Text);
+            int result;
+            if (a < b) Tukar(ref a, ref b);
+            if (!cekValid(a, b)) result = 0;
+            else
+            {
+                if (a > 25)
+                {
+                    result = kombinasi(24, 24, mods);
+                    result = (int)((((long)result) * pow(2, b - 24, mods)) % mods);
+                }
+                else result = kombinasi(a - 1, b, mods);
+            }
+            return result;
+        }
+        private void BtnHitung_Click(object sender, EventArgs e)
+        {
+            TxtHasil.Text = Hitung(a, b).ToString();
+        }
+
+            static bool cekValid(int a, int b)
+            {
+                if (a < 25) return false;
+                if (a == 25) return b <= 23;
+                return b == (a - 2);
+            }
+            public static void Tukar<T>(ref T left, ref T right)
+            {
+                T temp;
+                temp = left;
+                left = right;
+                right = temp;
+            }
+        public static int kombinasi(int n, int r, int mod)
+        {
+            if (n < r) Tukar(ref n, ref r);
+            if (n < 1 || r < 1) return 1;
+            var baris = new int[r];
+            for (var i = 0; i < r; baris[i++] = 1) { }
+            for (int i = 2; i < r; ++i)
+            {
+                for (int j = i - 1; j > 0; --j)
+                {
+                    baris[j] = (baris[j] + baris[j - 1]) % mod;
+                }
+            }
+            --r;
+            int jumlah = baris[r];
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = r; j > 0; --j)
+                {
+                    baris[j] = (baris[j] + baris[j - 1]) % mod;
+                }
+                jumlah = (jumlah + baris[r]) % mod;
+            }
+            return jumlah;
+        }
+        private static int pow(int baseNo, int exponent, int mod)
+        {
+            if (exponent < 2)
+            {
+                return (exponent < 1) ? 1 : baseNo;
+            }
+            long product = pow(baseNo, exponent >> 1, mod);
+            product = (product * product) % mod;
+            return ((exponent % 1) == 1) ? (int)((product * baseNo) % mod) : (int)(product);
+        }
+        
+    }
+}
